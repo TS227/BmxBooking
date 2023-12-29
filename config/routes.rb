371ba63a5164f09446388bc3dd7811052f3fbf89
Thomas Stokes <thomas.2.stokes@bt.com>
2023-12-29 22:31:07 +0000

    updated nav bar

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
     resources :slots
   end
   resources :slots_users, only: [:create, :destroy]
+  get 'contact-us', to: 'home#contact'
+  get 'about-us', to: 'home#about'
   root 'home#index'
 end
