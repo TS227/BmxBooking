60cc754e659f627711812c4bc7d9d56ed2b79ef7
Thomas Stokes <thomas.2.stokes@bt.com>
2023-12-29 17:19:57 +0000

    adds events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,32 @@
 class EventsController < ApplicationController
   def index
+    @events = Event.all
   end
+
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    @event.save
+    redirect_to @event
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def destroy
+    @event = Event.find(params[:id]).destroy
+    redirect_to events_path
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:event_name, :event_date)
+
+  end
+
 end
